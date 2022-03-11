05af5674bbc2e5de19e735088d50e6e97771b2a2
JeffreyDvk <jeffrey.dvk.fr@gmail.com>
2022-03-11 01:32:10 +0100

    Init

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- *main - entry point
+ * main - Entry point
  *
- *Return: value 1
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, ch, sizeof(ch)-1);
-    return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
