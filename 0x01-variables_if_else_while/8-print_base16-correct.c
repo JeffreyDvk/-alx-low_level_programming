9d0e0fa72403ad240ecf8f799685da74c22913cc
JeffreyDvk <jeffrey.dvk.fr@gmail.com>
2022-03-12 18:31:00 +0100

    C Variables if else ...

--- 0x01-variables_if_else_while/8-print_base16-correct.c
+++ 0x01-variables_if_else_while/8-print_base16-correct.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - main block
- * Description: prints all single digit numbers of base 10
- * starting from 0, followed by a new line.
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char d = '0';
+	int n;
+	char l;
 
-	while (d <= '9')
-	{
-		putchar(d);
-		d++;
-	}
-
-	char c = 'a';
-
-	while (c <= 'f')
-	{
-		putchar(c);
-		c++;
-	}
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
 
 	putchar('\n');
-	
+
 	return (0);
 }
